93f1dbd94f6371498aefd862ff0e1263470e523d
kimojin <114929941+kimojin@users.noreply.github.com>
2022-12-11 10:20:24 +0900

    Update chess

--- Chess/chess.js
+++ Chess/chess.js
@@ -1,31 +1,76 @@
 function rank(r){
-	let rank = ["1", "2", "3", "4", "5", "6", "7", "8"];
+	let rank = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
 	
-	if ((r >= 0) && (r <= 7))
-		return rank[r];
-	else
-		return 0;
+	return rank[r];
 }
 
 function file(f){
-	let file = ["a", "b", "c", "d", "e", "f", "g", "h"];
+	let file = ["x", "a", "b", "c", "d", "e", "f", "g", "h"];
 	
-	if ((f >= 0) && (f <= 7))
-		return file[f];
-	else
-		return 0;
+	return file[f];
 }
 
 function not(r, f){
-	if (((f >= 0) && (f <= 7)) && ((r >= 0) && (r <= 7)))
-		return `${file(f)}${rank(r)}`;
+	return `${file(f)}${rank(r)}`;
+}
+
+function coord(r, f){
+	if (((f > 0) && (f < 9)) && ((r > 0) && (r < 9)))
+		return (r - 1) * 8 + f;
 	else
 		return 0;
 }
 
-function sq(r, f){
-	if (((f >= 0) && (f <= 7)) && ((r >= 0) && (r <= 7)))
-		return r * 8 + f;
+function notToCoord(not){
+	for(let r = 0; r < 9; r++){
+		for(let f = 0; f < 9; f++){
+			if(`${file(f)}${rank(r)}` == not)
+				return coord(r, f);
+		}
+	}
+	
+	return 0;
+}
+
+function coordToNot(coord){
+	for(let r = 0; r < 9; r++){
+		for(let f = 0; f < 9; f++){
+			if((r - 1) * 8 + f == coord)
+				return not(r, f);
+		}
+	}
+	
+	return "x0";
+}
+
+function getSquareByName(name){
+	for(let i = 0; i < 64; i++){
+		if(name == square[ + 1].name)
+			return square[i + 1];
+	}
+	
+	return square[0];
+}
+
+function getSquareByPiece(p){
+	if(p)
+		return square[p.square];
 	else
-		return 0;
+		return square[0];
+}
+
+function getPieceByName(name){
+	for(let i = 0; i < 32; i++){
+		if(name == piece[i + 1].name)
+			return piece[i + 1];
+	}
+	
+	return piece[0];
+}
+
+function getPieceByCoord(coord){
+	if((coord >= 0) && (coord <= 64))
+		return piece[square[coord].piece];
+	else
+		return piece[0];
 }
